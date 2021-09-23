04c35f6b1d8c838ec551d21c18e7781f45817078
Powell <preston.powell@nebo.edu>
2021-09-23 14:19:42 -0600

    Worked on Projectile

--- Prototype 2 - Top Down Game/Assets/Scripts/PlayerController.cs
+++ Prototype 2 - Top Down Game/Assets/Scripts/PlayerController.cs
@@ -4,20 +4,53 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 2.0f;
+    public float speed = 5.0f;
     public float turnSpeed = 10.0f;
     private float hInput;
     private float vInput;
 
+    public float xRange = 9.17f;
+    public float yRange = 4.45f;
+
+    public GameObject projectile;
+    public Vector3 offset = new Vector3(0,1,0);
+
     // Update is called once per frame
     void Update()
     {
-       // Gathring Keyboard input for movement
+       // Gathering Keyboard input for movement
        hInput = Input.GetAxis("Horizontal");
        vInput = Input.GetAxis("Vertical");
        // Makes the PC rotate left and right 
        transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime * hInput);
        // Makes the PC move forward and back
-       transform.Translate(Vector3.forward * speed * Time.deltaTime * vInput); 
+       transform.Translate(Vector3.up * speed * Time.deltaTime * vInput);
+        // Creates right wall 
+        if(transform.position.x > xRange)
+        {
+          transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        // Creates left wall 
+        if(transform.position.x < -xRange)
+        {
+          transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        // Creates top wall 
+        if(transform.position.y > yRange)
+        {
+          transform.position = new Vector3(transform.position.x, -yRange, transform.position.z);
+        }
+        // Creates bottom wall 
+        if(transform.position.y < -yRange)
+        {
+          transform.position = new Vector3(transform.position.x, 
+          yRange, transform.position.z);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+          Instantiate(projectile, transform.position + offset, projectile.transform.rotation);
+        }
+        
     }
 }
