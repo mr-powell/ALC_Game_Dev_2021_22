97a85fea244df8cabf22e808825a04be76d55771
Powell <preston.powell@nebo.edu>
2021-10-26 15:01:19 -0600

    Added Object Pool Functionality

--- Prototype 3 - First Person Game A/Assets/Scripts/Weapons.cs
+++ Prototype 3 - First Person Game A/Assets/Scripts/Weapons.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Weapons : MonoBehaviour
 {
 
-    public GameObject bulletPrefab;
+    public ObjectPool bulletPool;
     public Transform muzzle;
 
     public float bulletSpeed;
@@ -44,7 +44,9 @@ public class Weapons : MonoBehaviour
       lastShootTime = Time.time;
       curAmmo--; 
 
-      GameObject bullet = Instantiate(bulletPrefab, muzzle.position, muzzle.rotation);
+      GameObject bullet = bulletPool.GetObject();
+      bullet.transform.position = muzzle.position;
+      bullet.transform.rotation = muzzle.rotation;
 
       // set the velocity
       bullet.GetComponent<Rigidbody>().velocity = muzzle.forward * bulletSpeed; 
